5b6705ce308a8997f3f7bb8b0c268367772aac87
dev-ahad-ali <ahadali.dev@gmail.com>
2026-01-13 00:35:37 +0600

    Update: axios updated

--- utils/axiosSecure.ts
+++ utils/axiosSecure.ts
@@ -5,4 +5,19 @@ const axiosSecure = axios.create({
   withCredentials: true,
 });
 
+axiosSecure.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    if (error.response?.status === 401) {
+      await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/refresh`,
+        {},
+        { withCredentials: true }
+      );
+      return axiosSecure(error.config);
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default axiosSecure;
