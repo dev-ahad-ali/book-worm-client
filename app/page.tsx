11c7f5f5fe3ec17c738b5aa6e234237ac6aa8669
dev-ahad-ali <ahadali.dev@gmail.com>
2026-01-14 23:13:25 +0600

    Fix: provider tsx file fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,16 @@
+'use client';
+
+import { useAppDispatch } from '@/lib/hooks';
+import { loadUser } from '@/lib/slices/authSlice';
+import { useEffect } from 'react';
+
 export default function Home() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(loadUser());
+  }, [dispatch]);
+
   return (
     <div className='flex min-h-screen items-center justify-center'>
       <div className='text-center'>
--- lib/providers.tsx
+++ lib/providers.tsx
@@ -4,16 +4,7 @@ import type React from 'react';
 
 import { Provider } from 'react-redux';
 import { store } from '@/lib/store';
-import { useAppDispatch } from './hooks';
-import { useEffect } from 'react';
-import { loadUser } from './slices/authSlice';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    dispatch(loadUser());
-  }, [dispatch]);
-
   return <Provider store={store}>{children}</Provider>;
 }
